6896fd221835e8ebb73d4320c757722fffdd04e1
Celularfreak <82029406+celularfreak@users.noreply.github.com>
2024-03-16 11:16:55 +0100

    Author class

---
+++ src/main/java/com/example/library/demo/model/Author.java
@@ -0,0 +1,53 @@
+package com.example.library.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer authorId;
+    private String name;
+    private String email;
+    private Book authorBook;
+
+    public Author(String name, String email, Book authorBook) {
+        setName(name);
+        setEmail(email);
+        setAuthorBook(authorBook);
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Book getAuthorBook() {
+        return authorBook;
+    }
+
+    public void setAuthorBook(Book authorBook) {
+        this.authorBook = authorBook;
+    }
+
+}
