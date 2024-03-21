9e1482f19b11525af3fdbe07eee9253602dcfe9a
Anna de Pablo Puig <71819431+annadpp@users.noreply.github.com>
2024-03-21 12:03:40 +0100

    Book class (fixed upload)

--- src/main/java/com/example/library/demo/model/Book.java
+++ src/main/java/com/example/library/demo/model/Book.java
@@ -1,2 +1,83 @@
-package com.example.library.demo.model;public class Book {
+package com.example.library.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @Column(name = "isbn", unique = true)
+    private String isbn;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    //Parametrized constructor
+    public Book(String isbn, String title, String category, Integer quantity) {
+        setIsbn(isbn);
+        setTitle(title);
+        setCategory(category);
+        setQuantity(quantity);
+    }
+
+    //Getters & Setters
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        if (isbn == null || isbn.isEmpty()) {
+            throw new IllegalArgumentException("ISBN cannot be null or empty");
+        }
+
+        // Remove any hyphens or spaces from the ISBN
+        isbn = isbn.replaceAll("[\\s-]+", "");
+
+        //Check whether ISBN-10 or ISBN-13
+        if (!isbn.matches("\\d{10}") && !isbn.matches("\\d{13}")) {
+            throw new IllegalArgumentException("Invalid ISBN format");
+        }
+
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
+
