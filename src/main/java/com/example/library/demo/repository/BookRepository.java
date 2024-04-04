49f8de5ad2b7c431029f8d52f608cdc1ef6e9d9b
lausaru <laurasanchezru@gmail.com>
2024-04-04 20:55:45 +0200

    tests for bookrepository

--- src/main/java/com/example/library/demo/repository/BookRepository.java
+++ src/main/java/com/example/library/demo/repository/BookRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
-    List<Book> findAllByTitle(String title);
-    List<Book> findAllByCategory(String category);
+    Optional<List<Book>> findAllByTitle(String title);
+    Optional<List<Book>> findAllByCategory(String category);
 }
