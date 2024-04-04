b2cbae462ad018adff3aa6b92abcf9f32449fdc4
lausaru <laurasanchezru@gmail.com>
2024-04-04 19:20:55 +0200

    commands added; BookService created and methods implemented in BookRepository

--- src/main/java/com/example/library/demo/repository/BookRepository.java
+++ src/main/java/com/example/library/demo/repository/BookRepository.java
@@ -3,5 +3,9 @@ package com.example.library.demo.repository;
 import com.example.library.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> findAllByTitle(String title);
+    List<Book> findAllByCategory(String category);
 }
