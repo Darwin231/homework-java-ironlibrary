209b9876f8b8262b26babf0189ced9d300006d17
“wardin231” <didiazhernandez@gmail.com>
2024-04-04 19:53:55 +0200

    Commands 4,6 and 7

--- src/main/java/com/example/library/demo/repository/BookRepository.java
+++ src/main/java/com/example/library/demo/repository/BookRepository.java
@@ -1,7 +1,23 @@
 package com.example.library.demo.repository;
 
+import com.example.library.demo.model.Author;
 import com.example.library.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    @Query("SELECT b FROM Book b WHERE b.Book.Author= :Author")
+    List<Book> findAllByAuthor(@Param("Author") String author);
+
+    List<Object[]> findAllBookAndAuthor();
+
+    @Query("SELECT b FROM Book b WHERE b.Book.Student= :Student")
+    List<Book> findAllByUsn(@Param("Student") String student);
+
 }
